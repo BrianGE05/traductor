39fb17a9287eb66160b92b0c5dbdb92430d20ef4
BrianGE05 <157042822+BrianGE05@users.noreply.github.com>
2024-05-28 18:38:22 -0600

    Add files via upload

---
+++ traducir.php
@@ -0,0 +1,33 @@
+<?php 
+$palabra_ingles=$_POST['variable_php'];
+if($palabra_ingles == "cat"){
+echo("Gato");
+}  
+else if($palabra_ingles == "dog"){
+    echo("perro");
+    }  
+    else if($palabra_ingles == "red"){
+        echo("rojo");
+        }  
+        else if($palabra_ingles == "run"){
+             echo("correr");
+            }  
+            else if($palabra_ingles == "city"){
+                         echo("ciudad");
+                }  
+                else if($palabra_ingles == "country"){
+                    echo("pais");
+                    }  
+                    else if($palabra_ingles == "take"){
+                        echo("tomar")
+                        }  
+                        else if($palabra_ingles == "bad"){
+                            echo("malo");
+                            }  
+                            else if($palabra_ingles == "time"){
+                                echo("tiempo");
+                                }  
+                                else if($palabra_ingles == "one"){
+                                    echo("uno");
+                                    }
+?>
